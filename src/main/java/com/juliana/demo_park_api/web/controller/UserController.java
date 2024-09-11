6c1ad1a6e92de24000f7e2f2406486e2d5e906a6
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-09-11 16:52:38 -0300

    springdoc - getbyid

--- src/main/java/com/juliana/demo_park_api/web/controller/UserController.java
+++ src/main/java/com/juliana/demo_park_api/web/controller/UserController.java
@@ -6,14 +6,20 @@ import com.juliana.demo_park_api.web.dto.ResponseDto;
 import com.juliana.demo_park_api.web.dto.UserCreateDto;
 import com.juliana.demo_park_api.web.dto.UserPasswordDto;
 import com.juliana.demo_park_api.web.dto.mapper.UserMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name = "Users", description = "It contains operations for register, update and read for users")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/users")
@@ -21,11 +27,28 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "Create a new user", description = "Resource to create a new user",
+            responses = {
+                @ApiResponse(responseCode = "201", description = "Resource created!",
+                        content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDto.class))),
+                @ApiResponse(responseCode = "409", description = "Username already registered in the system",
+                        content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                @ApiResponse(responseCode = "422", description = "Resource not processed by invalid data",
+                        content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     public ResponseEntity<ResponseDto> create(@Valid @RequestBody UserCreateDto userDto) {
         User obj = userService.save(UserMapper.toUser(userDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toDto(obj));
     }
+
+    @Operation(summary = "Get a user by id", description = "Resource to get user by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Resource found!",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Resource not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping("/{id}")
     public ResponseEntity<ResponseDto> getById(@PathVariable Long id) {
         User user = userService.searchById(id);
